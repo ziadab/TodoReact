14b85163e85dc5a5f5240b44939acdcd0511adb5
Ziad Abouelfarah <zain.fake02@gmail.com>
2019-11-13 22:59:15 +0100

    makeDone function

--- src/components/Todos.js
+++ src/components/Todos.js
@@ -1,23 +1,24 @@
-import React from 'react';
-import TodosItem from './TodosItem';
-import PropTypes from 'prop-types';
+import React from "react";
+import TodosItem from "./TodosItem";
+import PropTypes from "prop-types";
 
-class Todos extends React.Component{
-    render(){
-        return (
-            <div>
-                {this.props.todos.map((todo) => (
-                    <TodosItem
-                    todo={todo} 
-                    key={todo.id}/>
-                ))}
-            </div>
-        )
-    }
+class Todos extends React.Component {
+  markDone = () => {
+    console.log("Thla! ");
+  };
+  render() {
+    return (
+      <div>
+        {this.props.todos.map(todo => (
+          <TodosItem todo={todo} key={todo.id} markDone={this.markDone} />
+        ))}
+      </div>
+    );
+  }
 }
 
 Todos.propTypes = {
-    todos : PropTypes.array.isRequired
-}
+  todos: PropTypes.array.isRequired
+};
 
-export default Todos
+export default Todos;
--- src/components/TodosItem.js
+++ src/components/TodosItem.js
@@ -1,34 +1,31 @@
-import React, { Component } from 'react'
-import PropTypes from 'prop-types';
+import React, { Component } from "react";
+import PropTypes from "prop-types";
 
 export class TodosItem extends Component {
+  getStyle = () => {
+    return {
+      padding: "5px",
+      borderBottom: "1px #fff solid",
+      textDecoration: this.props.todo.done ? "line-through" : "none",
+      backgroundColor: "#282c34",
+      color: "#fff    "
+    };
+  };
 
-    getStyle = () => {
-        return {
-            padding: "5px",
-            borderBottom: "1px #fff solid",
-            textDecoration: this.props.todo.done ? "line-through": "none",
-            backgroundColor: "#282c34",
-            color: '#fff    '
-        }
-    }
-
-    render() {
-        return (
-            
-            <div  style={this.getStyle()}>
-                <p>
-                    <input type="checkbox"/>{" "}
-                    {this.props.todo.title}
-                </p>
-            </div>
-
-        )
-    }
+  render() {
+    return (
+      <div style={this.getStyle()}>
+        <p>
+          <input type="checkbox" onChange={this.props.markDone} />{" "}
+          {this.props.todo.title}
+        </p>
+      </div>
+    );
+  }
 }
 
 TodosItem.propTypes = {
-    todo : PropTypes.object.isRequired
-}
+  todo: PropTypes.object.isRequired
+};
 
-export default TodosItem
+export default TodosItem;
