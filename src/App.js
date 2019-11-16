3b644e26af5768d9da1d39344c7de2ea9b1c1d89
Ziad Abouelfarah <zain.fake02@gmail.com>
2019-11-16 22:34:59 +0100

    close to finish

--- src/App.js
+++ src/App.js
@@ -2,11 +2,18 @@ import React from "react";
 import Todos from "./components/Todos";
 import Header from "./components/layout/Header"
 import AddToDo from "./components/AddToDo";
+import uuid from "uuid";
 
 class App extends React.Component {
 
   addTodo = (title) => {
     console.log(title);
+    const newTodo = {
+      title: title,
+      id: uuid.v4(),
+      done: false
+    }
+    this.setState({todos : [...this.state.todos, newTodo]})
   }
 
   del = (id) => {
@@ -29,17 +36,17 @@ class App extends React.Component {
   state = {
     todos: [
       {
-        id: 1,
+        id: uuid.v4(),
         title: "Press F to pay respect",
         done: false
       },
       {
-        id: 2,
+        id: uuid.v4(),
         title: "Toxicity",
         done: false
       },
       {
-        id: 3,
+        id: uuid.v4(),
         title: "PSY",
         done: false
       }
