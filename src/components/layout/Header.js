98387978693fc0dc6ecab54067693fd8382dc2e9
Ziad Abouelfarah <zain.fake02@gmail.com>
2019-11-17 22:18:57 +0100

    yuuumi this shit is going good

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,4 +1,5 @@
 import React from "react"
+import {Link} from "react-router-dom"
 
 const header = {
     background : "#132126",
@@ -7,10 +8,16 @@ const header = {
     textAlign: "center"
 }
 
+const links = {
+    color: "#fff",
+    textDecoration: "none"
+}
+
 function Header(){
     return(
         <header style={header}>
             <h1>TODO YA 3AM</h1>
+            <Link style={links} to="/">Home</Link> | <Link style={links} to="/about">About</Link>
         </header>
     )
 }
